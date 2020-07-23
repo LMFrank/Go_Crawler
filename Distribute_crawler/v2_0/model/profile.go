822752a16309040f9c573464dd198bcf6c5bbc4a
Xushuchi <njuxushuchi@163.com>
2020-07-23 16:18:05 +0800

    delete

--- Distribute_crawler/v2_0/model/profile.go
+++ Distribute_crawler/v2_0/model/profile.go
@@ -2,18 +2,17 @@ package model
 
 import "strconv"
 
-type Profile struct {
-	Name          string
-	Marriage      string
-	Age           int
-	Constellation string
-	Height        int
-	JobAddress    string
-	Salary        string
-	Edu           string
+type Bookdetails struct {
+	Bookname string
+	Author   string
+	Press    string
+	Pages    int
+	Price    string
+	Score    string
+	Intro    string
 }
 
-func (p Profile) String() string {
-	return p.Name + " " + p.Marriage + " " + strconv.Itoa(p.Age) + "岁 " +
-		strconv.Itoa(p.Height) + "cm " + p.JobAddress + " 月收入：" + p.Salary + " 学历：" + p.Edu
+func (b Bookdetails) String() string {
+	return "\n书名：" + b.Bookname + "\n作者：" + b.Author + "\n出版社：" + b.Press + "\n页数：" +
+		strconv.Itoa(b.Pages) + "\n价格：" + b.Price + "\n评分：" + b.Score + "\n简介：" + b.Intro
 }
